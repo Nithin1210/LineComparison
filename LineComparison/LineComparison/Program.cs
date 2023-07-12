5d744e674a8d90b9672c202111b88d24fe306638
Takkoli Nithin Kumar Reddy <nithinkumarreddy.t@tvsnext.com>
2023-07-12 15:04:09 +0530

    [Nithin] Add:Completion of Double

---
+++ LineComparison/LineComparison/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace LineComparison
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Operation o1 = new Operation(1, 2, 3, 4);
+            double line1 = o1.CalculateLength();
+            Console.WriteLine(line1);
+
+            Operation o2 = new Operation(2, 3, 4, 8);
+            double line2 = o2.CalculateLength();
+            Console.WriteLine(line2);
+
+        }
+    }
+
+
+}
+
